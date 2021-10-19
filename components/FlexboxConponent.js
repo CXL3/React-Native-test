3fdb6ca6d20f719789c785bd895b9e2d9293dbe7
Claire Lu <appsfunlu@gmail.com>
2021-10-18 23:38:13 -0700

    show how to use flaxbox

--- components/FlexboxConponent.js
+++ components/FlexboxConponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Text, View } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
 
 class Flexbox extends Component {
   static navigationOptions = {
@@ -7,17 +7,47 @@ class Flexbox extends Component {
   };
   render() {
     return (
-      <View>
-        <Text>Test</Text>
-        <Text>Test</Text>
-        <Text>Test</Text>
-        <Text>Test</Text>
-        <Text>Test</Text>
-        <Text>Test</Text>
-        <Text>Test</Text>
+      <View style={styles.container}>
+        <Text style={styles.box1}>1</Text>
+        <Text style={styles.box2}>2</Text>
+        <Text style={styles.box3}>3</Text>
+        <Text style={styles.box4}>4</Text>
       </View>
     );
   }
 }
 
 export default Flexbox;
+
+const styles = StyleSheet.create({
+  container: {
+    // flex:1,
+    flexDirection:"row",
+    // flexDirection:"column",
+    // justifyContent also can do flex-start, flex-end, space between, center, space-around
+    justifyContent: 'center',
+    alignItems: "flex-end",
+    paddingTop:40,
+    backgroundColor:'#ddd',
+  },
+  box1:{
+    backgroundColor:'gold',
+    padding:10,
+    flex:2,
+  },
+  box2:{
+    backgroundColor:'red',
+    padding:20,
+    flex:1,
+  },
+  box3:{
+    backgroundColor:'blue',
+    padding:30,
+    flex:1,
+  },
+  box4:{
+    backgroundColor:'green',
+    padding:40,
+    flex:1,
+  }
+})
