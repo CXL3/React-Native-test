dba38dd0b48d84d5232e62368bce00c7b771a041
Claire Lu <appsfunlu@gmail.com>
2021-11-01 22:58:17 -0700

    reversed the previous change. I will make a demo for Nav 5 seperately

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,172 +1,164 @@
+
 import React, { Component } from "react";
-// import Home from "./HomeComponent";
+import Home from "./HomeComponent";
 import Directory from "./DirectoryComponent";
 import CampsiteInfo from "./CampsiteInfoComponent";
-// import Reservation from "./Form";
+import Reservation from "./Form";
 import Constants from "expo-constants";
 import {
   View,
   Platform,
   StyleSheet,
-  // Text,
-  // ScrollView,
-  // Image,
+  Text,
+  ScrollView,
+  Image,
 } from "react-native";
-import { createStackNavigator } from "@react-navigation/stack";
-// import { createDrawerNavigator, DrawerItems } from "react-navigation-drawer";
-// import { createAppContainer } from "react-navigation";
-// import { Icon } from "react-native-elements";
-// import SafeAreaView from "react-native-safe-area-view";
-
-const Stack = createStackNavigator();
-
-const Main = () => (
-  <Stack.Navigator>
-    <Stack.Screen name="Directory" component={Directory} />
-    <Stack.Screen name="CampsiteInfo" component={CampsiteInfo} />
-  </Stack.Navigator>
-);
+import { createStackNavigator } from "react-navigation-stack";
+import { createDrawerNavigator, DrawerItems } from "react-navigation-drawer";
+import { createAppContainer } from "react-navigation";
+import { Icon } from "react-native-elements";
+import SafeAreaView from "react-native-safe-area-view";
 
 //Stack navigator
-// const DirectoryNavigator = createStackNavigator(
-//   {
-//     Directory: {
-//       screen: Directory,
-//       //add list icon on the directory page
-//       navigationOptions: ({ navigation }) => ({
-//         headerLeft: (
-//           <Icon
-//             name="list"
-//             type="font-awesome"
-//             iconStyle={styles.stackIcon}
-//             onPress={() => navigation.toggleDrawer()}
-//           />
-//         ),
-//       }),
-//     },
-//     CampsiteInfo: { screen: CampsiteInfo },
-//   },
-//   {
-//     initialRouteName: "Directory",
-//     defaultNavigationOptions: {
-//       headerStyle: {
-//         backgroundColor: "#000000",
-//       },
-//       headerTintColor: "#fff",
-//       headerTitleStyle: {
-//         color: "#fff",
-//       },
-//     },
-//   }
-// );
+const DirectoryNavigator = createStackNavigator(
+  {
+    Directory: {
+      screen: Directory,
+      //add list icon on the directory page
+      navigationOptions: ({ navigation }) => ({
+        headerLeft: (
+          <Icon
+            name="list"
+            type="font-awesome"
+            iconStyle={styles.stackIcon}
+            onPress={() => navigation.toggleDrawer()}
+          />
+        ),
+      }),
+    },
+    CampsiteInfo: { screen: CampsiteInfo },
+  },
+  {
+    initialRouteName: "Directory",
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: "#000000",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
+    },
+  }
+);
 
-// const HomeNavigator = createStackNavigator(
-//   {
-//     Home: { screen: Home },
-//   },
-//   {
-//     defaultNavigationOptions: ({ navigation }) => ({
-//       headerStyle: {
-//         backgroundColor: "#000000",
-//       },
-//       headerTintColor: "#fff",
-//       headerTitleStyle: {
-//         color: "#fff",
-//       },
-//       headerLeft: (
-//         <Icon
-//           name="home"
-//           type="font-awesome"
-//           iconStyle={styles.stackIcon}
-//           onPress={() => navigation.toggleDrawer()}
-//         />
-//       ),
-//     }),
-//   }
-// );
-// const ReservationNavigator = createStackNavigator(
-//   {
-//     Reservation: { screen: Reservation },
-//   },
-//   {
-//     defaultNavigationOptions: ({ navigation }) => ({
-//       headerStyle: {
-//         backgroundColor: "#5637DD",
-//       },
-//       headerTintColor: "#fff",
-//       headerTitleStyle: {
-//         color: "#fff",
-//       },
-//       headerLeft: (
-//         <Icon
-//           name="tree"
-//           type="font-awesome"
-//           iconStyle={styles.stackIcon}
-//           onPress={() => navigation.toggleDrawer()}
-//         />
-//       ),
-//     }),
-//   }
-// );
+const HomeNavigator = createStackNavigator(
+  {
+    Home: { screen: Home },
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      headerStyle: {
+        backgroundColor: "#000000",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
+      headerLeft: (
+        <Icon
+          name="home"
+          type="font-awesome"
+          iconStyle={styles.stackIcon}
+          onPress={() => navigation.toggleDrawer()}
+        />
+      ),
+    }),
+  }
+);
+const ReservationNavigator = createStackNavigator(
+  {
+    Reservation: { screen: Reservation },
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      headerStyle: {
+        backgroundColor: "#5637DD",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
+      headerLeft: (
+        <Icon
+          name="tree"
+          type="font-awesome"
+          iconStyle={styles.stackIcon}
+          onPress={() => navigation.toggleDrawer()}
+        />
+      ),
+    }),
+  }
+);
 
-// const CustomDrawerContentComponent = (props) => (
-//   <ScrollView>
-//     <SafeAreaView
-//       style={styles.container}
-//       forceInset={{ top: "always", horizontal: "never" }}
-//     >
-//       <View style={styles.drawerHeader}>
-//         <View style={{ flex: 1 }}>
-//           <Image
-//             source={require("./images/bmw1.jpg")}
-//             style={styles.drawerImage}
-//           />
-//         </View>
-//         <View style={{ flex: 2 }}>
-//           <Text style={styles.drawerHeaderText}>BMW</Text>
-//         </View>
-//       </View>
-//       <DrawerItems {...props} />
-//     </SafeAreaView>
-//   </ScrollView>
-// );
-// //Drawer navigator
-// const MainNavigator = createDrawerNavigator(
-//   {
-//     Home: {
-//       screen: HomeNavigator,
-//       navigationOptions: {
-//         drawerIcon: ({ tintColor }) => (
-//           <Icon name="home" type="font-awesome" size={24} color={tintColor} />
-//         ),
-//       },
-//     },
-//     Directory: {
-//       screen: DirectoryNavigator,
-//       navigationOptions: {
-//         drawerIcon: ({ tintColor }) => (
-//           <Icon name="list" type="font-awesome" size={24} color={tintColor} />
-//         ),
-//       },
-//     },
-//     Reservation: {
-//       screen: ReservationNavigator,
-//       navigationOptions: {
-//         drawerLabel: "Buy one today!",
-//         drawerIcon: ({ tintColor }) => (
-//           <Icon name="tree" type="font-awesome" size={24} color={tintColor} />
-//         ),
-//       },
-//     },
-//   },
-//   {
-//     drawerBackgroundColor: "#ffffff",
-//     contentComponent: CustomDrawerContentComponent,
-//   }
-// );
+const CustomDrawerContentComponent = (props) => (
+  <ScrollView>
+    <SafeAreaView
+      style={styles.container}
+      forceInset={{ top: "always", horizontal: "never" }}
+    >
+      <View style={styles.drawerHeader}>
+        <View style={{ flex: 1 }}>
+          <Image
+            source={require("./images/bmw1.jpg")}
+            style={styles.drawerImage}
+          />
+        </View>
+        <View style={{ flex: 2 }}>
+          <Text style={styles.drawerHeaderText}>BMW</Text>
+        </View>
+      </View>
+      <DrawerItems {...props} />
+    </SafeAreaView>
+  </ScrollView>
+);
+//Drawer navigator
+const MainNavigator = createDrawerNavigator(
+  {
+    Home: {
+      screen: HomeNavigator,
+      navigationOptions: {
+        drawerIcon: ({ tintColor }) => (
+          <Icon name="home" type="font-awesome" size={24} color={tintColor} />
+        ),
+      },
+    },
+    Directory: {
+      screen: DirectoryNavigator,
+      navigationOptions: {
+        drawerIcon: ({ tintColor }) => (
+          <Icon name="list" type="font-awesome" size={24} color={tintColor} />
+        ),
+      },
+    },
+    Reservation: {
+      screen: ReservationNavigator,
+      navigationOptions: {
+        drawerLabel: "Buy one today!",
+        drawerIcon: ({ tintColor }) => (
+          <Icon name="tree" type="font-awesome" size={24} color={tintColor} />
+        ),
+      },
+    },
+  },
+  {
+    drawerBackgroundColor: "#ffffff",
+    contentComponent: CustomDrawerContentComponent,
+  }
+);
 
-// // const AppNavigator = createAppContainer(DirectoryNavigator);
-// const AppNavigator = createAppContainer(MainNavigator);
+// const AppNavigator = createAppContainer(DirectoryNavigator);
+const AppNavigator = createAppContainer(MainNavigator);
 
 class Main extends Component {
   render() {
@@ -175,6 +167,7 @@ class Main extends Component {
         style={{
           flex: 1,
           paddingTop: Platform.OS === "ios" ? 0 : Constants.statusBarHeight,
+          
         }}
       >
         <AppNavigator />
